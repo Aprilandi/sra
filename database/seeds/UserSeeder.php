1caf0dc1cde9f7f3c4704ccb24c74fb428006e4f
Ferianrh <fastercorner@gmail.com>
2020-02-28 17:53:26 +0700

    add seeder for table  roles and users

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'id_role' => '1',
+            'username' => 'admin',
+            'name' => 'Admin',
+            'email'=>'admin@mail.com',
+            'password' => bcrypt('admin123'),
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('users')->insert([
+            'id_role' => '2',
+            'username' => 'santri',
+            'name' => 'Santri',
+            'email'=>'santri@mail.com',
+            'password' => bcrypt('santri123'),
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+    }
+}
